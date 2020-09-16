3dd33c704a434c93d2cfd4d23125da19af440079
Sean Swanson <seancswan@gmail.com>
2020-09-15 19:14:07 -0700

    Now rendering wild fire map and AQI value. Added loading spinner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms'
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavComponent } from './nav/nav.component';
 import { ContentComponent } from './content/content.component';
 import { GaugeModule } from 'angular-gauge';
+import { NgxSpinnerModule } from "ngx-spinner";
 
 @NgModule({
   declarations: [
@@ -17,8 +20,11 @@ import { GaugeModule } from 'angular-gauge';
   imports: [
     BrowserModule,
     FormsModule,
-    GaugeModule.forRoot()
+    GaugeModule.forRoot(),
+    BrowserAnimationsModule,
+    NgxSpinnerModule,
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [GaugeModule],
   bootstrap: [AppComponent]
 })
--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import AerisWeather from '@aerisweather/javascript-sdk';
+import { NgxSpinnerService } from "ngx-spinner";
 
 @Component({
   selector: 'app-content',
@@ -11,43 +12,60 @@ export class ContentComponent implements OnInit {
   data: any = {};
   dataOb: any = {};
   alertOb: any = {};
+  aqiOb: any = {};
   mapOb: any = {};
-  constructor() {
+
+  selectedLocation = this.buildLocation('seattle', 'wa');
+
+  constructor(private spinner: NgxSpinnerService) {
     this.getObservations();
     this.getAirQuality();
     this.getAlerts();
     this.getMap();
+
   }
 
   ngOnInit(): void {
+    this.spinner.show();
+
   }
 
   getObservations() {
-    this.aeris.api().endpoint('observations').place('98033').get().then((result) => {
+    this.aeris.api().endpoint('observations').place(this.selectedLocation).get().then((result) => {
       this.data = result.data;
       this.dataOb = result.data.ob;
     });
   }
 
   getAirQuality() {
-    this.aeris.api().endpoint('airquality').place('98033').get().then((result) => {
-      console.log(result)
+    this.aeris.api().endpoint('airquality').place(this.selectedLocation).get().then((result) => {
+      console.log(result.data[0].periods[0])
+      this.aqiOb = {
+        aqiValue: result.data[0].periods[0].aqi,
+        aqiLabel: result.data[0].periods[0].category
+      }
     });
   }
 
   getAlerts() {
-    this.aeris.api().endpoint('alerts').place('98033').get().then((result) => {
+    this.aeris.api().endpoint('alerts').place(this.selectedLocation).get().then((result) => {
       this.alertOb = result.data[0].details;
     });
   }
 
   getMap() {
-    this.aeris.map().layers('flat-dk,temperatures,water-flat-dk,counties-dk:60,admin-dk').center('seattle,wa').zoom(6).size(600, 400).get().then((result) => {
+    this.aeris.map().layers('flat-dk,terrain-dk,water-depth,admin-dk,air-quality').center(this.selectedLocation).zoom(6).size(600, 400).get().then((result) => {
       const { image, metadata: { validDate } } = result;
       if (image) {
         this.mapOb.imgSrc = image.src
       }
+      this.spinner.hide();
+
     }
     );
+  }
+
+  buildLocation(city, state) {
+    return `${city},${state}`;
   }
 }
