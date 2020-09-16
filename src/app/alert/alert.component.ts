3488deaf322e39fd9e3f80fcf2ee99b96d6c564d
Sean Swanson <seancswan@gmail.com>
2020-09-15 20:28:16 -0700

    Browser now remembers if you have dismissed the alert.

--- src/app/alert/alert.component.ts
+++ src/app/alert/alert.component.ts
@@ -9,6 +9,7 @@ export class AlertComponent implements OnInit {
   @Input() alertType: string;
   @Input() alertBody: string;
 
+  visible: boolean = this.getLocalValue('alertVisible');
   infoShown: boolean = false;
 
   constructor() { }
@@ -18,5 +19,18 @@ export class AlertComponent implements OnInit {
 
   toggleInfo() {
     this.infoShown = !this.infoShown;
+  }
+
+  clearAlert() {
+    this.visible = false;
+    window.localStorage.setItem('alertVisible', 'false');
+  }
+
+  getLocalValue(key) {
+    if (!window.localStorage.getItem('alertVisible')) {
+      return true;
+    } else {
+      return JSON.parse(window.localStorage.getItem('alertVisible'))
+    }
   }
 }
